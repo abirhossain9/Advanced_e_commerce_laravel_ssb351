bf1860fca0f295894d75c55daa2a04cc503e9695
Md Abir Hossain <abir.hossain04@northsouth.edu>
2021-08-17 17:06:36 +0600

    Branch addition
    Branch addition

--- app/resources/views/backend/includes/footer.blade.php
+++ app/resources/views/backend/includes/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="br-footer">
         <div class="footer-left">
-          <div class="mg-b-2">Copyright &copy; 2017. Bracket Plus. All Rights Reserved.</div>
-          <div>Attentively and carefully made by ThemePixels.</div>
+          <div class="mg-b-2">Copyright &copy; 2021. All Rights Reserved.</div>
+          <div>Attentively and carefully made by Abir Hossain.</div>
         </div>
         <div class="footer-right d-flex align-items-center">
           <span class="tx-uppercase mg-r-10">Share:</span>
--- app/routes/web.php
+++ app/routes/web.php
@@ -1,10 +1,25 @@
 <?php
 
+use App\Models\Backend\Branch;
 use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
-| Web Routes
+| Frontend Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+
+
+/*
+|--------------------------------------------------------------------------
+| Backend Admin Panel Web Routes
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
@@ -16,11 +31,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
+//this routes are for branch management
 Route::group(['prefix' => 'admin'], function(){
     Route::get('/dashboard', 'App\Http\Controllers\Backend\DashboardController@dashboard') ->name('admin.dashboard');
-    Route::group(['prefix' => 'branch'], function(){
-
-        });
+    Route::group(['prefix' => '/branch'], function(){
+        Route::get('/manage','App\Http\Controllers\Backend\BranchController@index')->name('branch.manage');
+        Route::get('/create','App\Http\Controllers\Backend\BranchController@create')->name('branch.create');
+        Route::post('/store','App\Http\Controllers\Backend\BranchController@store')->name('branch.store');
+        Route::post('/edit/{id}','App\Http\Controllers\Backend\BranchController@edit')->name('branch.edit');
+        Route::post('/update/{id}','App\Http\Controllers\Backend\BranchController@update')->name('branch.update');
+        Route::post('/destroy/{id}','App\Http\Controllers\Backend\BranchController@update')->name('branch.destroy');
+    });
 
 });
