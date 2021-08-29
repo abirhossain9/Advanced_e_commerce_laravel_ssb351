6907c81b7ed41f7cb3744980e69fbb086a768def
Md Abir Hossain <abir.hossain04@northsouth.edu>
2021-08-29 18:08:34 +0600

    Mentor.php updated
    Mentor.php updated

---
+++ app/app/Models/Backend/Mentor.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models\Backend;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Mentor extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'fullname',
+        'designation',
+        'slug',
+        'overview',
+        'phone',
+        'address',
+        'email',
+        'fiverr_img',
+        'fiverr_url',
+        'upwork_img',
+        'upwork_url',
+        'profile_pic'
+    ];
+}
