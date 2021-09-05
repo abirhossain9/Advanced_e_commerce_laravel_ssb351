8e5bc6dd7214443635d5cc97a37fffa4c3a6d3ce
Md Abir Hossain <abir.hossain04@northsouth.edu>
2021-09-05 18:53:52 +0600

    Database modeling done for course
    Database modeling done for course

--- app/database/migrations/2021_08_31_150056_create_courses_table.php
+++ app/database/migrations/2021_08_31_150056_create_courses_table.php
@@ -15,6 +15,22 @@ class CreateCoursesTable extends Migration
     {
         Schema::create('courses', function (Blueprint $table) {
             $table->id();
+            $table->string('english_title')->unique();
+            $table->string('bangla_title')->nullable();
+            $table->string('slug');
+            $table->string('intro_video')->nullable();
+            $table->integer('price');
+            $table->integer('bangla_price');
+            $table->integer('graduate_number');
+            $table->integer('total_lecture');
+            $table->integer('class_hour');
+            $table->integer('course_duration');
+            $table->string('motivational_content')->nullable();
+            $table->string('course_opportunity')->nullable();
+            $table->string('curriculam_description')->nullable();
+            $table->integer('cupon_status')->comment('1 for accept, 2 for not accept');
+            $table->integer('status')->comment('1 for active, 2 for inactive');
+            $table->text('image')->nullable();
             $table->timestamps();
         });
     }
