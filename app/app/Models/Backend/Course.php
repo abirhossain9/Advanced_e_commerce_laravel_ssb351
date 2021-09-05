5c198e9772f65419e716553701bb3f6cd4e26ada
Md Abir Hossain <abir.hossain04@northsouth.edu>
2021-09-05 18:59:04 +0600

    Model Updated for course
    Model Updated for course

--- app/app/Models/Backend/Course.php
+++ app/app/Models/Backend/Course.php
@@ -8,4 +8,22 @@ use Illuminate\Database\Eloquent\Model;
 class Course extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'english_title',
+        'bangla_title',
+        'slug',
+        'intro_video',
+        'price',
+        'bangla_price',
+        'graduate_number',
+        'total_lecture',
+        'class_hour',
+        'course_duration',
+        'motivational_content',
+        'course_opportunity',
+        'curriculam_description',
+        'cupon_status',
+        'status',
+        'image'
+    ];
 }
