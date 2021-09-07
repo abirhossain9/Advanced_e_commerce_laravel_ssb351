41265af012d2333c6a24a394fe6ac63172afbefa
Md Abir Hossain <abir.hossain04@northsouth.edu>
2021-09-07 20:25:04 +0600

    Manage Page for Course Curriculum #2
    Manage Page for Course Curriculum

---
+++ app/resources/views/backend/pages/curriculum/manage.blade.php
@@ -0,0 +1,120 @@
+@extends('backend.layout.template') @section('body')
+
+<div class="br-pagetitle">
+    <i class="icon ion-ios-home-outline"></i>
+    <div>
+        <h4>Manage All Curriculum</h4>
+        <p class="mg-b-0">Manage All Course Curriculum From Here.</p>
+    </div>
+</div>
+
+<div class="br-pagebody">
+    <div class="row">
+        <div class="col-lg-12">
+            {{-- page body content start --}}
+
+            <div class="card bd-0 shadow-base">
+                 @include('backend.flash-massage')
+                <div class="d-md-flex justify-content-between pd-25">
+
+                    {{-- Table Content start --}}
+                    <div class="bd bd-gray-300 rounded table-responsive">
+                        <table class="table table-bordered table-striped table-hover table-custom">
+                            <thead class="thead-dark">
+                                <tr>
+                                    <th scope="col">#sl.</th>
+                                    <th scope="col">Course Title</th>
+                                    <th scope="col">Status</th>
+                                    <th scope="col">Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @php $i = 0; @endphp
+                                @foreach($curriculums as $curriculum)
+                                @php $i++; @endphp
+
+                                <tr>
+                                    <th scope="row">{{$i;}}</th>
+                                    <td>{{$curriculum->fullname}}</td>
+                                    <td>
+                                        @if ($curriculum->status==1)
+                                        <span class="badge badge-success">active</span>
+                                        @elseif ($curriculum->status==2)
+                                        <span class="badge badge-danger">inactive</span>
+                                        @endif
+                                    </td>
+                                    <td>
+                                        <ul class="custom-action">
+                                            <li>
+                                                <a href="{{route('curriculum.edit',$curriculum->id)}}">
+                                                    <i class="fa fa-edit"></i>
+                                                </a>
+                                            </li>
+                                            <li>
+                                                <a href="" data-toggle="modal" data-target="#curriculum{{$curriculum->id}}">
+                                                    <i class="fa fa-trash "></i>
+                                                </a>
+                                            </li>
+                                        </ul>
+                                    </td>
+                                    <!-- Modal -->
+                                    <div
+                                        class="modal fade"
+                                        id="curriculum{{$curriculum->id}}"
+                                        tabindex="-1"
+                                        role="dialog"
+                                        aria-labelledby="exampleModalCenterTitle"
+                                        aria-hidden="true">
+                                        <div class="modal-dialog modal-dialog-centered" role="document">
+                                            <div class="modal-content">
+                                                <div class="modal-header">
+                                                    <h5 class="modal-title" id="exampleModalLongTitle">Delte This curriculum ?</h5>
+                                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                        <span aria-hidden="true">&times;</span>
+                                                    </button>
+                                                </div>
+                                                <div class="modal-body">
+                                                    <div class="modal-button text-center">
+                                                        <ul>
+                                                            <li>
+                                                                <form action="{{route('curriculum.destroy',$curriculum->id)}}" method="POST">
+                                                                    @csrf
+                                                                    <button type="submit" class="btn btn-danger">Confirm</button>
+                                                                </form>
+
+                                                            </li>
+                                                            <li>
+                                                                <button type="button" class="btn btn-success" data-dismiss="modal">Cancel</button>
+                                                            </li>
+                                                        </ul>
+
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </tr>
+                                @endforeach
+
+                            </tbody>
+                        </table>
+                        @if ($curriculums->count()==0)
+                            <div class="alert alert-info">
+                                No curriculum added please add a curriculum first
+                            </div>
+                        @endif
+
+                    </div>
+
+                    {{-- Table Content end --}}
+                    <!-- d-flex -->
+                </div>
+                <!-- d-flex -->
+            </div>
+            <!-- card -->
+
+            {{-- page body content end --}}
+        </div>
+    </div>
+</div>
+@endsection
