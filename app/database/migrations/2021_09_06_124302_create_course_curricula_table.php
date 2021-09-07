4bc38a082d52e969f9bead9be4fb50e86dc17632
Md Abir Hossain <abir.hossain04@northsouth.edu>
2021-09-07 19:41:45 +0600

    Database for course curriculum #2
    Database for course curriculum

--- app/database/migrations/2021_09_06_124302_create_course_curricula_table.php
+++ app/database/migrations/2021_09_06_124302_create_course_curricula_table.php
@@ -15,6 +15,23 @@ class CreateCourseCurriculaTable extends Migration
     {
         Schema::create('course_curricula', function (Blueprint $table) {
             $table->id();
+            $table->integer('course_id');
+            $table->string('one_en')->nullable();
+            $table->string('one_bn')->nullable();
+            $table->text('one_desc')->nullable();
+            $table->string('two_en')->nullable();
+            $table->string('two_bn')->nullable();
+            $table->text('two_desc')->nullable();
+            $table->string('three_en')->nullable();
+            $table->string('three_bn')->nullable();
+            $table->text('three_desc')->nullable();
+            $table->string('four_en')->nullable();
+            $table->string('four_bn')->nullable();
+            $table->text('four_desc')->nullable();
+            $table->string('five_en')->nullable();
+            $table->string('five_bn')->nullable();
+            $table->text('five_desc')->nullable();
+            $table->integer('status')->default(1)->comment('1 for active, 2 for inactive');
             $table->timestamps();
         });
     }
