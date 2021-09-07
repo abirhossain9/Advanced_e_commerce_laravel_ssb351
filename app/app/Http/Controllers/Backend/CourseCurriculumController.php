5e94b7c30f7f9e596231ae8ec8d9d32958d2bf9a
Md Abir Hossain <abir.hossain04@northsouth.edu>
2021-09-07 20:01:45 +0600

    create() updated for CourseCurriculumController #2

--- app/app/Http/Controllers/Backend/CourseCurriculumController.php
+++ app/app/Http/Controllers/Backend/CourseCurriculumController.php
@@ -29,7 +29,7 @@ class CourseCurriculumController extends Controller
      */
     public function create()
     {
-        //
+        return view('backend.pages.curriculum.create');
     }
 
     /**
