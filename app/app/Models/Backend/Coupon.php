cda4d8027e1936a2698211ed883a2273a591a9b0
Md Abir Hossain <abir.hossain04@northsouth.edu>
2021-09-12 20:01:20 +0600

    Coupon model updated #8

--- app/app/Models/Backend/Coupon.php
+++ app/app/Models/Backend/Coupon.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Coupon extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'code',
+        'discount_type',
+        'course_type',
+        'fixed_value',
+        'percent_value',
+        'status'
+    ];
 }
