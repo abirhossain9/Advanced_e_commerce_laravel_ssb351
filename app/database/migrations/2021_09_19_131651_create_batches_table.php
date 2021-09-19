1a42a19e97ac8534138c1d5fa9e89a4fa97042c1
Md Abir Hossain <abir.hossain04@northsouth.edu>
2021-09-19 19:49:42 +0600

    database #10

--- app/database/migrations/2021_09_19_131651_create_batches_table.php
+++ app/database/migrations/2021_09_19_131651_create_batches_table.php
@@ -15,6 +15,19 @@ class CreateBatchesTable extends Migration
     {
         Schema::create('batches', function (Blueprint $table) {
             $table->id();
+            $table->string('batch_name')->unique();
+            $table->string('slug');
+            $table->string('starting_date')->nullable();
+            $table->string('class_day')->nullable();
+            $table->string('class_timing')->nullable();
+            $table->string('fb_group')->nullable();
+            $table->string('sit_number')->default(25);
+            $table->integer('branch_id')->default(0);
+            $table->integer('course_id')->default(0);
+            $table->integer('mentor_id')->default(0);
+            $table->integer('batch_type')->default(1)->comment('1 = Online, 2 = Offline');
+            $table->integer('admission_status')->default(2)->comment('1 = Active, 2 = Inactive');
+            $table->integer('status')->default(2)->comment('1 for active, 2 for inactive');
             $table->timestamps();
         });
     }
