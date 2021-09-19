43e35fccd74da0ef8c755cf1f32ba1451bc8584b
Md Abir Hossain <abir.hossain04@northsouth.edu>
2021-09-19 19:49:53 +0600

    Model updated #10

--- app/app/Models/Backend/Batch.php
+++ app/app/Models/Backend/Batch.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class Batch extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'batch_name',
+        'slug',
+        'starting_date',
+        'class_day',
+        'class_timing',
+        'fb_group',
+        'sit_number',
+        'branch_id',
+        'course_id',
+        'mentor_id',
+        'batch_type',
+        'admission_status',
+        'status'
+    ];
 }
