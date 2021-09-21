7cebabf89ce47b8420e7dc39b00bc8772c00c109
Md Abir Hossain <abir.hossain04@northsouth.edu>
2021-09-21 21:01:56 +0600

    Batch update done #10

--- app/app/Http/Controllers/Backend/BatchController.php
+++ app/app/Http/Controllers/Backend/BatchController.php
@@ -106,7 +106,23 @@ class BatchController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $batch = Batch::find($id);
+        $batch->batch_name = $request->batch_name;
+        $batch->slug = Str::slug($request->batch_name);
+        $batch->course_id = $request->course_id;
+        $batch->mentor_id = $request->mentor_id;
+        $batch->branch_id = $request->branch_id;
+        $batch->starting_date = $request->starting_date;
+        $batch->class_day = $request->class_day;
+        $batch->class_timing = $request->class_timing;
+        $batch->fb_group = $request->fb_group;
+        $batch->sit_number = $request->sit_number;
+        $batch->batch_type = $request->batch_type;
+        $batch->admission_status = $request->admission_status;
+        $batch->status = $request->status;
+
+        $batch->save();
+        return redirect()->route('batch.manage')->with('success','Batch updated successfully');
     }
 
     /**
