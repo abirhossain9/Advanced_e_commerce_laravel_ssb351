5264247a59b42c4e1336aa732f0c9ba9207fbe96
Md Abir Hossain <abir.hossain04@northsouth.edu>
2021-10-04 13:12:08 +0600

    Header for forntend #12

---
+++ app/resources/views/frontend/includes/header.blade.php
@@ -0,0 +1,7 @@
+    <!--Metas-->
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <meta name="viewport" id="viewport" content="width=device-width; initial-scale=1">
+    <meta name="description" content="Teamboard Creative Template">
+    <title>Shikhbe Shobai</title>
