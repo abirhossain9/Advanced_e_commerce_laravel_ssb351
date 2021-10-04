549c9bfccd2d4f5cee913631cd335ee689c89874
Md Abir Hossain <abir.hossain04@northsouth.edu>
2021-10-04 13:16:27 +0600

    Footer Updated #12

---
+++ app/resources/views/frontend/includes/footer.blade.php
@@ -0,0 +1,73 @@
+
+            <!--Footer-->
+            <footer class="footer ">
+                <div class="footer-warpper ">
+                    <div class="footer-top">
+                        <div class="container ">
+                            <div class=" clearfix ">
+                                <div class="footer-bottom-content clearfix ">
+                                    <div class="row ">
+                                        <div class="col-lg-3 col-md-3">
+                                            <div class="logo-footer ">
+                                                <h5>Shikhbe Shobai</h5>
+                                            </div>
+                                            <div class="text-footer ">
+                                                <p>Shikhbe Shobai teaches you the skills you need to thrive and earn online - now and in the future.</p>
+                                            </div>
+                                        </div>
+                                        <div class="col-lg-3 col-md-3">
+                                            <h5>Let's Connect</h5>
+                                            <div class="text-footer ">
+                                                <p>Shikhbe Shobai Facebook Page Freelancers Community Graphic Designers Group Web Designers Group Online Support</p>
+                                            </div>
+                                        </div>
+                                        <div class="col-lg-3 col-md-3">
+                                            <h5>Useful Links</h5>
+                                            <ul class="list-menu ">
+                                                <li>
+                                                    <a href=" ">All Courses</a>
+                                                </li>
+                                                <li>
+                                                    <a href=" ">Events</a>
+                                                </li>
+                                                <li>
+                                                    <a href=" ">Contact</a>
+                                                </li>
+                                                <li>
+                                                    <a href=" ">Support</a>
+                                                </li>
+                                            </ul>
+                                        </div>
+
+                                        <div class="col-lg-3 col-md-3">
+                                            <h5>Social Links</h5>
+                                            <ul class="list-menu ">
+                                                <li>
+                                                    <a href=" ">Facebook</a>
+                                                </li>
+                                                <li>
+                                                    <a href=" ">LinkedIn</a>
+                                                </li>
+                                                <li>
+                                                    <a href=" ">Instagram</a>
+                                                </li>
+                                                <li>
+                                                    <a href=" ">Youtube</a>
+                                                </li>
+                                            </ul>
+                                        </div>
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="footer-bottom ">
+                        <!-- COPYRIGHT TEXT -->
+                        <div class="copyright ">
+                            <p>2020 Copyright © Shikhbe Shobai Solutions, All rights Reserved.</p>
+                        </div>
+                        <!-- COPYRIGHT TEXT -->
+                    </div>
+                </div>
+            </footer>
