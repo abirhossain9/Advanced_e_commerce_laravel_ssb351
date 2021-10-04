2752c6307e96a720132e3a38c70a2bdd353fbdd2
Md Abir Hossain <abir.hossain04@northsouth.edu>
2021-10-04 13:29:52 +0600

    Navbar and homepage updated #12

---
+++ app/resources/views/frontend/includes/nav.blade.php
@@ -0,0 +1,51 @@
+
+            <!-- Header -->
+            <header class="navbar-header">
+                <nav class="navbar navbar-expand-lg fixed-top">
+                    <div class="container">
+                        <a class="navbar-brand" href="index.html">
+                            <span><img style="width: 100px;margin-top: 10px;margin-bottom: 10px;" src="https://shikhbeshobai.com/public/assets/home/assets/images/Shikhbe-shobai-logo.png" alt=""></span>
+                        </a>
+                        <button class="navbar-toggler" type="button" id="sidebarCollapse">
+                            <span class="navbar-toggler-icon icofont-navigation-menu"></span>
+                        </button>
+                        <div class="navbar-collapse justify-content-center" id="sidebar">
+
+
+                            <ul class="navbar-nav">
+                                <li class="nav-item active">
+                                    <a class="nav-link section-scroll" href="index.html">Home</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link section-scroll" href="courses.html">Courses</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a data-scroll="" class="nav-link section-scroll" href="">About Us</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a data-scroll="" class="nav-link section-scroll" href="">Events</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a data-scroll="" class="nav-link section-scroll" href="">Contact</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a data-scroll="" class="nav-link section-scroll" href="">Comunity</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a data-scroll="" class="nav-link section-scroll" href="">LIVE</a>
+                                </li>
+
+                            </ul>
+                        </div>
+
+                        <div class="ml-auto ml-auto-mobile top-bar">
+
+                            <a href=""><i class="icofont-ui-user"></i>Login / Signup</a>
+                            <a href=""><i class="icofont-support"></i>Get Support</a>
+
+                        </div>
+                    </div>
+                </nav>
+            </header>
+            <!--Header-->
+s
---
+++ app/resources/views/frontend/pages/homepage.blade.php
@@ -0,0 +1,23 @@
+@section('body')
+<section class="newsletter">
+    <div class="container">
+        <div class="newsletter-inner row">
+            <div class="col-md-4">
+                <h2>Banani Branch</h2>
+                <p>House# 74 Road# 7, <br>Block# H, Banani, Dhaka-1213 <br>
+                Phone: 01758066329, 01758066325, 01872650686, 01872657169 </p>
+            </div>
+
+            <div class="col-md-4">
+                <h2>Mirpur Branch</h2>
+                <p>HB Enterprise Bhaban. <br>Plot-9, Road-4, Section -7, <br>Mirpur-Dhaka 1216 <br>Phone: 01872650688, 01872657167, 01872650685</p>
+            </div>
+
+            <div class="col-md-4">
+                <h2>Dhanmondi Branch</h2>
+                <p>House # 21/b, <br>Dhanmondi 27 old, New 16, <br>Dhaka 1205 Phone: 01870726738, 01717808205, 01872657168</p>
+            </div>
+        </div>
+    </div>
+</section>
+@endsection
