43eea36863db22911ab5a39228037626bab68709
Md Abir Hossain <abir.hossain04@northsouth.edu>
2021-10-04 13:13:35 +0600

    CSS updated #12

---
+++ app/resources/views/frontend/includes/css.blade.php
@@ -0,0 +1,29 @@
+    <!--External Stylesheets css-->
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <!--   icon font -->
+    <link rel="stylesheet" href="assets/css/icofont.min.css">
+    <!--Animate -->
+    <link rel="stylesheet" href="assets/css/animate.css">
+    <!--Slick -->
+    <link rel="stylesheet" href="assets/css/slick.css">
+    <!-- Owl -->
+    <link rel="stylesheet" href="assets/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="assets/css/owl.theme.default.min.css">
+
+    <!-- Magnific Popup-->
+    <link rel="stylesheet" href="assets/css/magnific-popup.css">
+    <!--Template Stylesheets css-->
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/responsive.css">
+    <!-- Fonts styles -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,600i,700,700i" rel="stylesheet">
+    <!-- Favicon -->
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="assets/images/Shikhbe-shobai-logo.png" type="image/x-icon">
+    <link rel="icon" href="assets/images/Shikhbe-shobai-logo.png" type="image/x-icon">
+    <script src="assets/js/modernizr.js"></script>
+    <!--[if lt IE 9]>
+      <script src="assets/js/html5shiv.min.js"></script>
+      <script src="assets/js/respond.min.js"></script>
+    <![endif]-->
