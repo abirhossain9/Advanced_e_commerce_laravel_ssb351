e88aa54cda153a9243cd2aae2444ef60c753c9f6
Md Abir Hossain <abir.hossain04@northsouth.edu>
2021-10-05 17:43:09 +0600

    Registration #14

--- app/resources/views/auth/register.blade.php
+++ app/resources/views/auth/register.blade.php
@@ -7,20 +7,41 @@
           <div class="signin-logo tx-center tx-28 tx-bold tx-inverse"><span class="tx-normal">[</span> bracket <span class="tx-info">plus</span> <span class="tx-normal">]</span></div>
           <div class="tx-center mg-b-40">The Admin Template For Perfectionist</div>
 
+        <!-- Validation Errors -->
+        <x-auth-validation-errors class="mb-4" :errors="$errors" />
+        <form method="POST" action="{{ route('register') }}">
+            @csrf
+          <!-- Name -->
           <div class="form-group">
-            <input type="text" class="form-control" placeholder="Enter your username">
-          </div><!-- form-group -->
-          <div class="form-group">
-            <input type="password" class="form-control" placeholder="Enter your password">
-          </div><!-- form-group -->
-          <div class="form-group">
-            <input type="password" class="form-control" placeholder="Enter your fullname">
-          </div><!-- form-group -->
+            <x-label for="name" :value="__('Name')" />
+            <x-input id="name" class="form-control" type="text" placeholder="Enter your username" name="name" :value="old('name')" required autofocus />
+        </div>
+
+        <!-- Email Address -->
+        <div class="form-group">
+            <x-label for="email" :value="__('Email')" />
+            <x-input id="email" class="form-control" type="email" placeholder="Enter your email" name="email" :value="old('email')" required />
+        </div>
+        <!-- Password -->
+        <div class="form-group">
+            <x-label for="password" :value="__('Password')" />
+            <x-input id="password" class="form-control" type="password" name="password" required placeholder="Enter your password" autocomplete="new-password" />
+        </div>
+
+        <!-- Confirm Password -->
+        <div class="form-group">
+            <x-label for="password_confirmation" :value="__('Confirm Password')" />
+            <x-input id="password_confirmation" class="form-control" type="password" placeholder="Confirm your password" name="password_confirmation" required />
+        </div>
 
           <div class="form-group tx-12">By clicking the Sign Up button below, you agreed to our privacy policy and terms of use of our website.</div>
-          <button type="submit" class="btn btn-info btn-block">Sign Up</button>
+          <div class="form-group">
+            <button type="submit" class="btn btn-info btn-block">Sign Up</button>
+          </div>
 
-          <div class="mg-t-40 tx-center">Not yet a member? <a href="" class="tx-info">Sign Up</a></div>
+        </form>
+
+          <div class="mg-t-40 tx-center">Already a member? <a href="{{route('login')}}" class="tx-info">Sign In Here</a></div>
         </div><!-- login-wrapper -->
       </div><!-- d-flex -->
       @endsection
