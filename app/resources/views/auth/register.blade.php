af8728f0c04c3435ef547d3978708fc7d23db35f
Md Abir Hossain <abir.hossain04@northsouth.edu>
2021-10-05 16:00:30 +0600

    register layout updated #14

--- app/resources/views/auth/register.blade.php
+++ app/resources/views/auth/register.blade.php
@@ -1,59 +1,26 @@
-<x-guest-layout>
-    <x-auth-card>
-        <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
-        </x-slot>
+@extends('layouts.adminlayouts')
 
-        <!-- Validation Errors -->
-        <x-auth-validation-errors class="mb-4" :errors="$errors" />
+@section('body')
+    <div class="d-flex align-items-center justify-content-center bg-br-primary ht-100v">
 
-        <form method="POST" action="{{ route('register') }}">
-            @csrf
+        <div class="login-wrapper wd-300 wd-xs-400 pd-25 pd-xs-40 bg-white rounded shadow-base">
+          <div class="signin-logo tx-center tx-28 tx-bold tx-inverse"><span class="tx-normal">[</span> bracket <span class="tx-info">plus</span> <span class="tx-normal">]</span></div>
+          <div class="tx-center mg-b-40">The Admin Template For Perfectionist</div>
 
-            <!-- Name -->
-            <div>
-                <x-label for="name" :value="__('Name')" />
+          <div class="form-group">
+            <input type="text" class="form-control" placeholder="Enter your username">
+          </div><!-- form-group -->
+          <div class="form-group">
+            <input type="password" class="form-control" placeholder="Enter your password">
+          </div><!-- form-group -->
+          <div class="form-group">
+            <input type="password" class="form-control" placeholder="Enter your fullname">
+          </div><!-- form-group -->
 
-                <x-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus />
-            </div>
+          <div class="form-group tx-12">By clicking the Sign Up button below, you agreed to our privacy policy and terms of use of our website.</div>
+          <button type="submit" class="btn btn-info btn-block">Sign Up</button>
 
-            <!-- Email Address -->
-            <div class="mt-4">
-                <x-label for="email" :value="__('Email')" />
-
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
-            </div>
-
-            <!-- Password -->
-            <div class="mt-4">
-                <x-label for="password" :value="__('Password')" />
-
-                <x-input id="password" class="block mt-1 w-full"
-                                type="password"
-                                name="password"
-                                required autocomplete="new-password" />
-            </div>
-
-            <!-- Confirm Password -->
-            <div class="mt-4">
-                <x-label for="password_confirmation" :value="__('Confirm Password')" />
-
-                <x-input id="password_confirmation" class="block mt-1 w-full"
-                                type="password"
-                                name="password_confirmation" required />
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
-                    {{ __('Already registered?') }}
-                </a>
-
-                <x-button class="ml-4">
-                    {{ __('Register') }}
-                </x-button>
-            </div>
-        </form>
-    </x-auth-card>
-</x-guest-layout>
+          <div class="mg-t-40 tx-center">Not yet a member? <a href="" class="tx-info">Sign Up</a></div>
+        </div><!-- login-wrapper -->
+      </div><!-- d-flex -->
+      @endsection
