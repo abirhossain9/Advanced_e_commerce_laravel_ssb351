4c106c9d85ab2195a66a950699b77f7f1a0bee7c
Md Abir Hossain <abir.hossain04@northsouth.edu>
2021-10-09 23:13:17 +0600

    Routing Updated #19

--- app/routes/web.php
+++ app/routes/web.php
@@ -17,6 +17,7 @@ use Illuminate\Support\Facades\Route;
 //     return view('welcome');
 // });
 Route::get('/','App\Http\Controllers\frontend\PagesController@index')->name('home');
+Route::get('/course-details','App\Http\Controllers\frontend\PagesController@coursedetails')->name('course.details');
 
 /*
 |--------------------------------------------------------------------------
