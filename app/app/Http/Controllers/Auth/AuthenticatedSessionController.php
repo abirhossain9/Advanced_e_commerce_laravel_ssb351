4310205dd9b37d9edc698ac80ca80beb2449cb53
Md Abir Hossain <abir.hossain04@northsouth.edu>
2021-10-09 20:28:14 +0600

    redirection updated #16

--- app/app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -32,7 +32,13 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerate();
 
-        return redirect()->intended(RouteServiceProvider::HOME);
+        // return redirect()->intended(RouteServiceProvider::HOME);
+        if(Auth::user()->role == 1){
+            return redirect()->intended(RouteServiceProvider::HOME);
+        }
+        else{
+            return redirect('/');
+        }
     }
 
     /**
