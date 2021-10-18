8c448c5205fd9f17faf0861085df14b14649d417
Md Abir Hossain <abir.hossain04@northsouth.edu>
2021-10-18 17:35:39 +0600

    Routing updated #22

--- app/routes/web.php
+++ app/routes/web.php
@@ -19,6 +19,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/','App\Http\Controllers\frontend\PagesController@index')->name('home');
 Route::get('/batch/{slug}','App\Http\Controllers\frontend\PagesController@coursedetails')->name('course.details');
 Route::get('/student-login','App\Http\Controllers\frontend\PagesController@stdLogin')->name('stdLogin');
+Route::get('/student-register','App\Http\Controllers\frontend\PagesController@stdRegister')->name('stdRegister');
+Route::get('/student-dashboard','App\Http\Controllers\frontend\PagesController@stdDashboard')->middleware(['auth','verified'])->name('stdDashboard');
 /*
 |--------------------------------------------------------------------------
 | Backend Admin Panel Web Routes
