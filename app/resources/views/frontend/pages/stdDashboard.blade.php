e975c323d8feb7e1d77fd67fbaeef146a8d4d5af
Md Abir Hossain <abir.hossain04@northsouth.edu>
2021-10-23 15:32:31 +0600

    Student Dashboard Design done #22

--- app/resources/views/frontend/pages/stdDashboard.blade.php
+++ app/resources/views/frontend/pages/stdDashboard.blade.php
@@ -12,7 +12,45 @@
             </div>
           </nav>
           <div class="tab-content" id="nav-tabContent">
-            <div class="tab-pane fade show active" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">S</div>
+            <div class="tab-pane fade show active" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">
+              <form action="{{route('student.update',Auth::user()->id'')}}" method="POST" enctype="multipart/form-data">
+                @csrf
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-4">
+                            <div class="users-avatar">
+                                @if (!@empty(Auth::user()->image))
+                                    <img src="{{asset('backend/img/user')}}/{{Auth::user()->image}}">
+                                @else
+                                    <img src="{{asset('backend/img/default.jpg')}}">
+                                @endif
+                                <h3>{{Auth::user()->name}}</h3>
+                            </div>
+                        </div>
+                        <div class="col-md-4" style="margin-top: 10px;">
+                            <div class="form-group">
+                                <input type="text" class="form-control" name="name" value="{{Auth::user()->name}}">
+                            </div>
+                            <div class="form-group">
+                                <input type="email" disabled class="form-control" name="email" value="{{Auth::user()->email}}">
+                            </div>
+                            <div class="form-group">
+                                <input type="text" class="form-control" name="phone" value="{{Auth::user()->phone}}" placeholder="Your Phone Number">
+                            </div>
+                            <div class="form-group">
+                                <input type="text" class="form-control" name="address" value="{{Auth::user()->address}}" placeholder="Your Address">
+                            </div>
+                            <div class="form-group">
+                                <input type="file" class="form-control-file" name="image" >
+                            </div>
+                            <div class="form-group">
+                                <input type="submit" class="btn btn-success custom-btn " name="update" value="Update changes">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+              </form>
+            </div>
             <div class="tab-pane fade" id="nav-course" role="tabpanel" aria-labelledby="nav-course-tab">..</div>
             <div class="tab-pane fade" id="nav-notice" role="tabpanel" aria-labelledby="nav-notice-tab">...</div>
           </div>
