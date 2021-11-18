1e952793ac76a53bad23c6d3bac4d6c3fe985bd2
Md Abir Hossain <abir.hossain04@northsouth.edu>
2021-11-18 23:23:44 +0600

    ssl commerz integrated #25

--- app/routes/web.php
+++ app/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SslCommerzPaymentController;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,6 +24,23 @@ Route::get('/student-register','App\Http\Controllers\frontend\PagesController@st
 Route::get('/student-dashboard','App\Http\Controllers\frontend\PagesController@stdDashboard')->middleware(['auth','verified'])->name('stdDashboard');
 Route::post('/student-dashboard/update/{id}','App\Http\Controllers\Frontend\UsersController@StudentUpdate')->name('student.update');
 Route::get('/batch/{slug}/admission','App\Http\Controllers\Frontend\PagesController@admission')->name('getadmission');
+
+
+// SSLCOMMERZ Start
+Route::get('/example1', [SslCommerzPaymentController::class, 'exampleEasyCheckout']);
+Route::get('/example2', [SslCommerzPaymentController::class, 'exampleHostedCheckout']);
+
+Route::post('/pay', [SslCommerzPaymentController::class, 'index']);
+Route::post('/pay-via-ajax', [SslCommerzPaymentController::class, 'payViaAjax']);
+
+Route::post('/success', [SslCommerzPaymentController::class, 'success']);
+Route::post('/fail', [SslCommerzPaymentController::class, 'fail']);
+Route::post('/cancel', [SslCommerzPaymentController::class, 'cancel']);
+
+Route::post('/ipn', [SslCommerzPaymentController::class, 'ipn']);
+//SSLCOMMERZ END
+
+
 
 /*
 |--------------------------------------------------------------------------
